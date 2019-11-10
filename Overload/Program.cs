e6483af9ef14a3ff3c95dc81358518cdcafd143b
Billy Tukija <billytukij@gmail.com>
2019-11-09 22:02:26 -0300

    Simple project using overload in c#

---
+++ Overload/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Overload
+{
+    public static class DialogBox
+    {
+        public static void Show()
+        {
+            //Do something here
+        }
+        public static void Show(bool ok)
+        { 
+           //Do something here
+        }
+
+        public static void Show(string message, bool ok, bool cancel)
+        {
+            //Do something here
+        }
+
+        public static void Show(bool ok, bool ignore, bool cancel)
+        {
+            //Do something here
+        }
+
+        public static void Show(string feedback, bool submit , bool yes, bool no)
+        {
+            //Do something here
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DialogBox.Show();
+            DialogBox.Show(true);
+            DialogBox.Show(false, true, false);
+
+            //etc..
+        }
+    }
+}
